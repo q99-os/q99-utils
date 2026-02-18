ac47f60bd4aafc5eb0eb5bf41c37cdc74393b891
Matias Ceballos <maticeba98@gmail.com>
2026-02-18 10:03:55 -0300

    change version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages, setup
 setup(
     name='q99-utils',
     packages=find_packages(include=['q99_utils']),
-    version='0.1.0',
+    version='0.1.1',
     description='Library of common q99 services',
     author='Q99',
     install_requires=["pydantic", "httpx"],
