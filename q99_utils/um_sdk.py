3f59aabc19b1b9ec6a1ab995fce007e3ff758fc7
Martin Pierangeli <martin.pierangeli@q99.ai>
2026-03-08 02:42:19 +0000

    Add patch_credential method to UserManagerSDK for updating credentials

--- q99_utils/um_sdk.py
+++ q99_utils/um_sdk.py
@@ -76,6 +76,10 @@ class UserManagerSDK:
 
         return await self._request(method="PUT", url=update_url, json=data.model_dump())
 
+    async def patch_credential(self, credential_id: str, data: dict):
+        patch_url = f"{USER_MANAGER_URL}/v1/credentials/{credential_id}/"
+        return await self._request(method="PATCH", url=patch_url, json=data)
+
     async def activate_credential(self, credential_id:str):
         patch_url = f"{USER_MANAGER_URL}/v1/credentials/{credential_id}/"
 
